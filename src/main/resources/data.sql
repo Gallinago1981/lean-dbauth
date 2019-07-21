d9dc0c03a8eff7eae917e67dfa59160970b20c8a
snipes81 <snipes81b-github@yahoo.co.jp>
2019-07-21 23:31:35 +0900

    test03のパスワード有効期限切れは誤りなので修正

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,5 +4,5 @@ INSERT INTO user_data VALUES
 --pw : pass02
 ,(2, 'test02', '$2a$10$scQpcG5s.WbFI5BlrIkBkecWYaUikawKhoGMJKrmxx5DPPiqul4Yq', 'パスワード再設定', DATEADD(MONTH, -1, CURRENT_DATE), '0', CURRENT_TIMESTAMP , CURRENT_TIMESTAMP )
 -- pw: pass03
-,(3, 'test03', '$2a$10$scQpcG5s.WbFI5BlrIkBkecWYaUikawKhoGMJKrmxx5DPPiqul4Yq', '無効ユーザ', DATEADD(MONTH, -1, CURRENT_DATE), '1', CURRENT_TIMESTAMP , CURRENT_TIMESTAMP )
+,(3, 'test03', '$2a$10$scQpcG5s.WbFI5BlrIkBkecWYaUikawKhoGMJKrmxx5DPPiqul4Yq', '無効ユーザ', DATEADD(MONTH, 1, CURRENT_DATE), '1', CURRENT_TIMESTAMP , CURRENT_TIMESTAMP )
 ;
